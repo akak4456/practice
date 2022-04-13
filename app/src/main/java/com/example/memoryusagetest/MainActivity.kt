69d1b239133a699871fea7d7b5758e67f97372d6
akak4456 <akak4456@naver.com>
2022-04-13 11:15:54 +0900

    기초 뼈대 생성함

--- app/src/main/java/com/example/memoryusagetest/MainActivity.kt
+++ app/src/main/java/com/example/memoryusagetest/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.memoryusagetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity  : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+        supportFragmentManager.beginTransaction().add(R.id.frameLayout, HomeFragment()).commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.page_home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frameLayout , HomeFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.page_tv -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frameLayout, TVFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.page_calendar -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frameLayout, CalendarFragment()).commitAllowingStateLoss()
+                return true
+            }
+        }
+
+        return false
     }
 }
