757f9d7009b7486145f93e3c7429ac2f3e4ef3bf
akak4456 <akak4456@naver.com>
2022-05-15 22:49:45 +0900

    2022.05.15

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var emojis: [String] = ["🚂", "🚀", "🚁", "🚜"]
+  
+  var body: some View {
+    HStack {
+      ForEach(emojis, id: \.self , content: { emoji in
+        CardView(content: emoji)
+      })
     }
+    .padding(.horizontal)
+    .foregroundColor(.red)
+    
+  }
+}
+
+struct CardView: View {
+  var content: String
+  @State var isFaceUp: Bool = true
+  
+  var body: some View {
+    ZStack {
+      let shape = RoundedRectangle(cornerRadius: 20)
+      if isFaceUp {
+        shape.fill().foregroundColor(.white)
+        shape.stroke(lineWidth: 3)
+        Text(content).font(.largeTitle)
+      } else {
+        shape.fill()
+      }
+    }
+    .onTapGesture {
+      isFaceUp = !isFaceUp
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
