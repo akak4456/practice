2cd12948d8f32d6089956313385d1ef33caff4fb
akak4456 <akak4456@naver.com>
2022-05-15 19:54:43 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Choi Ayoung on 2022/05/15.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
