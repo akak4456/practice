dd956d53da0ec635820ca125dfe19a9e51fd2678
akak4456 <akak4456@naver.com>
2022-05-23 07:42:29 +0900

    2022.05.23

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-  var emojis: [String] = ["🚗","🚕","🚙","🚌","🚎","🏎","🚓","🚑","🚒","🚐","🛻","🚚","🚛","🚜","🦽"
-                          ,"🦼","🛴","🚲","🛵","🏍","🛺","🚔","🚍","🚘"]
-  @State var emojiCount = 4
+  let viewModel: EmojiMemoryGame
+  
   var body: some View {
     
     VStack{
       ScrollView{
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))] ){
-          ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-            CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+          ForEach(viewModel.cards) { card in
+            CardView(card: card).aspectRatio(2/3, contentMode: .fit)
           }
         }
       }
@@ -30,28 +29,25 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-  var content: String
-  @State var isFaceUp: Bool = true
+  let card: MemoryGame<String>.Card
   
   var body: some View {
     ZStack {
       let shape = RoundedRectangle(cornerRadius: 20)
-      if isFaceUp {
+      if card.isFaceUp {
         shape.fill().foregroundColor(.white)
         shape.strokeBorder(lineWidth: 3)
-        Text(content).font(.largeTitle)
+        Text(card.content).font(.largeTitle)
       } else {
         shape.fill()
       }
-    }
-    .onTapGesture {
-      isFaceUp = !isFaceUp
     }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    let game = EmojiMemoryGame()
+    ContentView(viewModel: game)
   }
 }
