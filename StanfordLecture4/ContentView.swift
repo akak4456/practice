280f38bb9099260514d00778d7565a7454c5a54d
akak4456 <akak4456@naver.com>
2022-05-25 07:00:53 +0900

    Initial Commit

---
+++ StanfordLecture4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StanfordLecture4
+//
+//  Created by Choi Ayoung on 2022/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StanfordLecture4/StanfordLecture4App.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordLecture4App.swift
+//  StanfordLecture4
+//
+//  Created by Choi Ayoung on 2022/05/25.
+//
+
+import SwiftUI
+
+@main
+struct StanfordLecture4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
