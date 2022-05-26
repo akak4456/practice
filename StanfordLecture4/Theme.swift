ba08f97d8f62cbdfe0f520f70a53ad606a3dd906
akak4456 <akak4456@naver.com>
2022-05-26 20:28:57 +0900

    theme create

---
+++ StanfordLecture4/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  StanfordLecture4
+//
+//  Created by Choi Ayoung on 2022/05/26.
+//
+
+import Foundation
+
+struct Theme {
+  let name: String
+  let setOfEmojis: [String]
+  let numberOfPairsOfCards: Int
+  let color: String
+}
