c36e147a591466f3970317ff0bf5029a2941b378
akak4456 <akak4456@naver.com>
2022-08-04 10:07:26 +0900

    first commit

---
+++ app/src/test/java/com/example/myfirstunittest/EngineTest.kt
@@ -0,0 +1,28 @@
+package com.example.myfirstunittest
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class EngineTest {
+
+    private val engine = Engine(2000, 189, 15, false)
+
+    @Test
+    fun engineTurnsOn() {
+        engine.turnOn()
+
+        assertEquals(true, engine.isTurnedOn)
+        assertEquals(95, engine.temperature)
+    }
+
+    @Test
+    fun engineTurnsOff() {
+        engine.turnOn()
+        
+        engine.turnOff()
+
+        assertEquals(false, engine.isTurnedOn)
+        assertEquals(15, engine.temperature)
+    }
+}
