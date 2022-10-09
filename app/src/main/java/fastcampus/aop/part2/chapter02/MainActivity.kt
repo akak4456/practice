d87450de52cecc0faee167d43a80a9a966df7886
akak4456 <akak4456@naver.com>
2022-10-09 14:12:37 +0900

    2022.10.09

--- app/src/main/java/fastcampus/aop/part2/chapter02/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/chapter02/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
@@ -65,9 +66,10 @@ class MainActivity : AppCompatActivity() {
 
                 textView.text = number.toString()
                 textView.isVisible = true
+
+                setNumberBackground(number, textView)
             }
 
-            Log.d("MainActivity", list.toString())
         }
     }
 
@@ -92,7 +94,23 @@ class MainActivity : AppCompatActivity() {
             textView.isVisible = true
             textView.text = numberPicker.value.toString()
 
+            setNumberBackground(numberPicker.value, textView)
+
             pickNumberSet.add(numberPicker.value)
+        }
+    }
+
+    private fun setNumberBackground(number: Int, textView: TextView) {
+        when (number) {
+            in 1..10 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_yellow)
+            in 11..20 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 -> textView.background =
+                ContextCompat.getDrawable(this, R.drawable.circle_gray)
+            else -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)
         }
     }
 
