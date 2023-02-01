9e232191f7619965a6d11672517986a1d716df7f
akak4456 <akak4456@naver.com>
2023-02-01 20:04:16 +0900

    2023.02.01

--- app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
@@ -1,9 +1,19 @@
 package fastcampus.aop.part3.chapter01
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private val firebaseToken: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
