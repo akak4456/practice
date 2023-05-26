5f00299e2f779286f8c40dbec1cb08e657581c4b
akak4456 <akak4456@naver.com>
2023-05-26 21:08:53 +0900

    addNumberSequence 함수 구성함

--- app/src/main/java/com/jo/calculatetdd/MainActivity.kt
+++ app/src/main/java/com/jo/calculatetdd/MainActivity.kt
@@ -4,27 +4,43 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.jo.calculatetdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
         setOnClickListeners()
     }
 
     private fun setOnClickListeners() {
-        findViewById<TextView>(R.id.button_no_1).setOnClickListener {
-            addNumber("1")
-        }
-        findViewById<TextView>(R.id.button_no_2).setOnClickListener {
-            addNumber("2")
+        listOf(
+            binding.buttonNo1,
+            binding.buttonNo2,
+            binding.buttonNo3,
+            binding.buttonNo4,
+            binding.buttonNo5,
+            binding.buttonNo6,
+            binding.buttonNo7,
+            binding.buttonNo8,
+            binding.buttonNo9,
+            binding.buttonNo0,
+        ).forEach { tv ->
+            tv.setOnClickListener {
+              addNumber(tv.text.toString())
+            }
         }
     }
 
     @SuppressLint("SetTextI18n")
     private fun addNumber(number: String) {
         val tv = findViewById<TextView>(R.id.tv_input)
-        tv.text = tv.text.toString() + number
+        var newText = tv.text.toString() + number
+        newText = newText.replace(",", "")
+        newText = "%,d".format(newText.toInt())
+        tv.text = newText
     }
 }
