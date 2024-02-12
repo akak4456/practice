d8f5fb1e1fe9e81d79772d8fc26ef4861dcbfe54
akak4456 <akak4456@naver.com>
2024-02-13 08:58:10 +0900

    2024.02.13

---
+++ LazyListDemo/app/src/main/java/com/example/lazylistdemo/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.example.lazylistdemo
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.example.lazylistdemo.ui.theme.LazyListDemoTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    private var itemArray: Array<String>? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        itemArray = resources.getStringArray(R.array.car_array)
+        super.onCreate(savedInstanceState)
+        setContent {
+            LazyListDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen(itemArray = itemArray as Array<out String>)
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MainScreen(itemArray: Array<out String>) {
+    val context = LocalContext.current
+    val groupedItems = itemArray.groupBy { it.substringBefore(' ') }
+
+    val onListItemClick = { text: String ->
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
+
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val displayButton = listState.firstVisibleItemIndex > 5
+    Box {
+        LazyColumn(state = listState, contentPadding = PaddingValues(bottom = 40.dp)) {
+            groupedItems.forEach { (manufacturer, models) ->
+                stickyHeader {
+                    Text(
+                        text = manufacturer,
+                        color = Color.White,
+                        modifier = Modifier
+                            .background(Color.Gray)
+                            .padding(5.dp)
+                            .fillMaxWidth()
+                    )
+                }
+                items(models.size) { modelIndex ->
+                    MyListItem(item = models[modelIndex], onItemClick = onListItemClick)
+                }
+            }
+
+        }
+        AnimatedVisibility(visible = displayButton, Modifier.align(Alignment.BottomCenter)) {
+            OutlinedButton(
+                onClick = { coroutineScope.launch { listState.scrollToItem(0) } },
+                border = BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.DarkGray
+                ),
+                modifier = Modifier.padding(5.dp)
+            ) {
+                Text(text = "Top")
+
+            }
+        }
+    }
+}
+
+@Composable
+fun MyListItem(item: String, onItemClick: (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable { onItemClick(item) },
+        shape = RoundedCornerShape(10.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            ImageLoader(item = item)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = item,
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun ImageLoader(item: String) {
+    val url =
+        "https://www.ebookfrenzy.com/book_examples/car_logos/" + item.substringBefore(" ") + "_logo.png"
+
+    Image(
+        painter = rememberImagePainter(url),
+        contentDescription = "car image",
+        contentScale = ContentScale.Fit,
+        modifier = Modifier.size(75.dp)
+    )
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    val itemArray: Array<String> = arrayOf("Cadillac Eldorado", "Ford Fairlane", "Plymouth Fury")
+    LazyListDemoTheme {
+        MainScreen(itemArray = itemArray)
+    }
+}
---
+++ ListDemo/app/src/main/java/com/example/listdemo/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.listdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.listdemo.ui.theme.ListDemoTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ListDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    RowList()
+}
+
+
+@Composable
+fun ColumnList() {
+    val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+    Column {
+        Row {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollTo(0)
+                    }
+                }, modifier = Modifier
+                    .weight(0.5f)
+                    .padding(2.dp)
+            ) {
+                Text(text = "Top")
+            }
+
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollTo(scrollState.maxValue)
+                    }
+                }, modifier = Modifier
+                    .weight(0.5f)
+                    .padding(2.dp)
+            ) {
+                Text(text = "End")
+            }
+        }
+        Column(Modifier.verticalScroll(scrollState)) {
+            repeat(500) {
+                Text(
+                    text = "List Item $it",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(5.dp)
+                )
+
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    ListDemoTheme {
+        MainScreen()
+    }
+}
+
+@Composable
+fun RowList() {
+    val scrollState = rememberScrollState()
+
+    Row(Modifier.horizontalScroll(scrollState)) {
+        repeat(50) {
+            Text(
+                text = " $it",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(5.dp)
+            )
+        }
+    }
+}
