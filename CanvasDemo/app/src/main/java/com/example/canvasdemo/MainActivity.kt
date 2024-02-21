cb26c5750cd1fa6bb153576ddf89f47285d4d4b7
akak4456 <akak4456@naver.com>
2024-02-22 06:43:26 +0900

    2024.02.22

--- CanvasDemo/app/src/main/java/com/example/canvasdemo/MainActivity.kt
+++ CanvasDemo/app/src/main/java/com/example/canvasdemo/MainActivity.kt
@@ -11,11 +11,23 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.inset
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.canvasdemo.ui.theme.CanvasDemoTheme
+import kotlin.math.PI
+import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +48,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    DrawLine()
+    DrawPoints()
 }
 
 @Composable
@@ -51,7 +63,181 @@ fun DrawLine() {
             start = Offset(x = 0f, y = 0f),
             end = Offset(x = width, y = height),
             color = Color.Blue,
-            strokeWidth = 16.0f
+            strokeWidth = 16.0f,
+            pathEffect = PathEffect.dashPathEffect(floatArrayOf(30f, 10f, 10f, 10f), phase = 0f)
         )
+    }
+}
+
+@Composable
+fun DrawRect() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        rotate(45f) {
+            drawRect(
+                color = Color.Blue,
+                topLeft = Offset(200f, 200f),
+                size = size / 2f
+            )
+        }
+    }
+}
+
+@Composable
+fun DrawCircle() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+
+        drawCircle(
+            color = Color.Blue,
+            center = center,
+            radius = 120.dp.toPx()
+        )
+    }
+}
+
+@Composable
+fun DrawOval() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+        drawOval(
+            color = Color.Blue,
+            topLeft = Offset(x = 25.dp.toPx(), y = 90.dp.toPx()),
+            size = Size(
+                width = canvasHeight - 50.dp.toPx(),
+                height = canvasHeight / 2 - 50.dp.toPx()
+            ),
+            style = Stroke(width = 12.dp.toPx())
+        )
+    }
+}
+
+@Composable
+fun GradientFill() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val canvasSize = size
+        val colorList: List<Color> = listOf(
+            Color.Red, Color.Blue, Color.Magenta,
+            Color.Yellow, Color.Green, Color.Cyan
+        )
+
+        val brush = Brush.horizontalGradient(
+            colors = colorList,
+            startX = 0f,
+            endX = 300.dp.toPx(),
+            tileMode = TileMode.Repeated
+        )
+
+        drawRect(
+            brush = brush,
+            size = canvasSize
+        )
+    }
+}
+
+@Composable
+fun RadialFill() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+        val radius = 150.dp.toPx()
+        val colorList: List<Color> = listOf(
+            Color.Red, Color.Blue,
+            Color.Magenta, Color.Yellow, Color.Green, Color.Cyan
+        )
+
+        val brush = Brush.radialGradient(
+            colors = colorList,
+            center = center,
+            radius = radius,
+            tileMode = TileMode.Repeated
+        )
+
+        drawCircle(
+            brush = brush,
+            center = center,
+            radius = radius
+        )
+    }
+}
+
+@Composable
+fun ShadowCircle() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val radius = 150.dp.toPx()
+        val colorList: List<Color> = listOf(Color.Blue, Color.Black)
+
+        val brush = Brush.horizontalGradient(
+            colors = colorList,
+            startX = 0f,
+            endX = 300.dp.toPx(),
+            tileMode = TileMode.Repeated
+        )
+
+        drawCircle(
+            brush = brush,
+            radius = radius
+        )
+    }
+}
+
+@Composable
+fun DrawArc() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        drawArc(
+            Color.Blue,
+            startAngle = 20f,
+            sweepAngle = 90f,
+            useCenter = true,
+            size = Size(250.dp.toPx(), 250.dp.toPx())
+        )
+    }
+}
+
+@Composable
+fun DrawPath() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val path = Path().apply {
+            moveTo(0f, 0f)
+            quadraticBezierTo(50.dp.toPx(), 200.dp.toPx(), 300.dp.toPx(), 300.dp.toPx())
+            lineTo(270.dp.toPx(), 100.dp.toPx())
+            quadraticBezierTo(60.dp.toPx(), 80.dp.toPx(), 0f, 0f)
+            close()
+        }
+
+        drawPath(
+            path = path,
+            Color.Blue
+        )
+    }
+}
+
+@Composable
+fun DrawPoints() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val height = size.height
+        val width = size.width
+        val points = mutableListOf<Offset>()
+
+        for (x in 0..size.width.toInt()) {
+            val y = (sin(x * (2f * PI / width)) * (height / 2) + height / 2).toFloat()
+            points.add(Offset(x.toFloat(), y))
+        }
+
+        drawPoints(
+            points = points,
+            strokeWidth = 3f,
+            pointMode = PointMode.Points,
+            color = Color.Blue
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    CanvasDemoTheme {
+        MainScreen()
     }
 }
