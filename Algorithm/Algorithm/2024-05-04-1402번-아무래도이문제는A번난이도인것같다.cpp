141ce93022a13b92ef7bf23ccc755a411fc5f886
akak4456 <akak4456@naver.com>
2024-05-04 13:52:06 +0900

    2024.05.04

---
+++ Algorithm/Algorithm/2024-05-04-1402번-아무래도이문제는A번난이도인것같다.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include<cmath>
+#include<string>
+#include<queue>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int T;
+	cin >> T;
+	while (T--) {
+		int A, B;
+		cin >> A >> B;
+		cout << "yes\n";
+	}
+	return 0;
+}
